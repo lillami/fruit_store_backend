b1da76016fa629fa199b75576a2b036dde879ec2
Lilla <40830933+lillami@users.noreply.github.com>
2020-11-09 14:57:45 +0100

    Creating the reports
    With no additional (only super-basic) text formatting.

---
+++ reports.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+Using the reportlab Python library, define the method generate_report to build the PDF reports.
+Create a PDF report named processed.pdf.
+'''
+
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
+
+
+def generate_method(filename, title, info):
+  styles = getSampleStyleSheet()
+  report = SimpleDocTemplate(filename)
+  report_title = Paragraph(title, styles["h1"])
+  report_info = Paragraph(info, styles["BodyText"])
+  empty_line = Spacer(1,20)
+
+  report.build([report_title, empty_line, report_info, empty_line])
