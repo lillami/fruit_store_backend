54c4d454dfd77425587a37fb25fc25e5c0bec723
Lilla <40830933+lillami@users.noreply.github.com>
2020-11-20 13:58:24 +0100

    Corrected the path

--- supplier_image__upload.py
+++ supplier_image__upload.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
 '''
-copy paste example_upload.py
-
-takes the jpeg images from the ____supplier-data/images directory____ that you've processed previously
-and uploads them to the ____web server fruit catalog____'''
+takes the jpeg images from the __supplier-data/images directory__ processed previously
+and uploads them to the __web server fruit catalog__
+'''
 
 import requests, os
 
 url = "http://localhost/upload/"
-path = '~/supplier-data/images'
+path = 'supplier-data/images/'
 pics = os.listdir(path)
 
 for pic in pics:
-    with open('~/supplier-data/images', 'rb') as opened:
+    with open('path+pic', 'rb') as opened:
         r = requests.post(url, files={'file': opened})
