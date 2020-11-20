9deb3ba86772ff292093b2331817f9b8f0bb8c22
Lilla <40830933+lillami@users.noreply.github.com>
2020-11-20 13:55:13 +0100

    Update changeImage.py
    Made it simpler + there was a problem with the order of the image conversion.

--- changeImage.py
+++ changeImage.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python3
 
-'''Final exam 1st part
-
-upload the new images to the online store
-images
-descriptions
-using: 2 different web endpoints'''
-
 #transforming and converting images
 import os, sys
 from PIL import Image
 
-path = '~/supplier-data/images'
+path = 'supplier-data/images/'
 content = os.listdir(path)
 
 for infile in content:
-    f, extension = os.path.splitext(infile)
-    outfile = f + ".jpeg"
-    if infile != outfile:
+    f = os.path.splitext(infile)[0]
+    outfile = path + f + ".jpeg"
         try:
-            with Image.open("~/images/", mode='r') as im:
-                im.rotate(90).resize((600,400)).save("~/supplier-data/images", format = ".jpeg").convert("RGBA", "RGB")
+            with Image.open(path+infile).rotate(90).resize((600,400)).convert("RGB").save(outfile, format = "JPEG")
         except OSError:
             print("cannot convert", infile)
