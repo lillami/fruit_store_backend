9d6240e476d9f9f4d6bb47a10082410361fe1843
Lilla <40830933+lillami@users.noreply.github.com>
2020-11-23 17:54:27 +0100

    Update report_email.py
    Re-wrote main()

--- report_email.py
+++ report_email.py
@@ -10,6 +10,8 @@ and the file path of the PDF to be generated as the attachment argument
 import os
 import datetime
 import reports
+import sys
+import emails
 
 
 def generate_pdf(path):
@@ -22,14 +24,27 @@ def generate_pdf(path):
         inline = f.readlines()
         name = inline[0].strip()
         weight = inline[1].strip()
-        pdf += "name:" + name + "<br>" + "weight:" + weight + "<br><br>"
+        pdf += "name: " + name + "<br/>" + "weight: " + weight + "<br/><br/>"
 
   return pdf
 
-
-if __name__ == "__main__":
+def main(argv):
     path = '/supplier-data/descriptions/'
     #process the data
     paragraph = generate_pdf(path)#text description processed from the text files
     title = "Process Update on the {}".format(date.today())
     reports.generate_report('/tmp/processed.pdf', title, paragraph)
+
+    #parameters for generating emails
+    sender = "automation@example.com"
+    receiver = "{}@example.com".format(os.environ["USER"])
+    subject = "Upload Completed - Online Fruit Store"
+    body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+    attachment = "/tmp/processed.pdf"
+
+    #generating emails
+    message = emails.generate_email(sender, receiver, subject, body, attachment)
+    emails.send_email(message)
+
+if __name__ == "__main__":
+    main(sys.argv)
